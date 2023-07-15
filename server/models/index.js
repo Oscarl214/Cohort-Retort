ea532460106c5a530c53ae3987e72df3f8c64e00
Oscar Leal <oscarleal234@gmail.com>
2023-07-14 20:11:44 -0500

    created modals+queries

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,5 @@
 const User = require("./User");
+const Comment = require("./Comment");
+const Post = require("./Post");
 
-module.exports = { User, Product, Category, Order };
+module.exports = { User, Comment, Post };
