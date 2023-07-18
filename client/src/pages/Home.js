9109a56600d9ed23c8e8cbb1ba31d7b350ea8523
sth <sth0067@gmail.com>
2023-07-18 18:29:50 -0500

    updated component file names

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import Nav from "../components/Nav";
+import CommunityContainer from "../components/CommunityContainer";
 
 const Home = () => {
-  return <div className="container"></div>;
+  return (
+    <div>
+      <Nav />
+      <CommunityContainer>
+        
+      </CommunityContainer>
+    </div>
+  );
 };
 
 export default Home;
