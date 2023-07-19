31747df4cd2288b9567c8d6de1b06802e928b5e2
sth <sth0067@gmail.com>
2023-07-19 12:11:56 -0500

    reorganized component nesting

---
+++ client/src/components/PostViewContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PostCard from "./PostCard";
+
+const PostViewContainer = () => {
+  return (
+    <div className="">
+        <PostCard />
+    </div>
+  );
+};
+
+export default PostViewContainer;
