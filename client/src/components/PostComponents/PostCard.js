b25dc008da48528540fb4290f1ed285685d5b726
Oscar Leal <oscarleal234@gmail.com>
2023-07-20 17:25:19 -0500

    fetching post data

--- client/src/components/PostComponents/PostCard.js
+++ client/src/components/PostComponents/PostCard.js
@@ -1,61 +1,76 @@
-import React, { useContext } from "react";
-import { Link } from "react-router-dom";
-import { UserContext } from "../../utils/userContext";
-
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_POSTS } from "../../utils/queries";
 import PostHeader from "./PostHeader";
-//make a parent component that uses the user query
-//fetch info we need
-//bring it in to our postCard Component
-//use it accordingly
 
 const PostCard = () => {
-  const { usersData } = useContext(UserContext);
+  // Fetch all posts using the QUERY_POSTS query
+  const { data, loading, error } = useQuery(QUERY_POSTS);
 
-  if (!usersData) {
+  if (loading) {
     return <p>Loading...</p>;
   }
 
-  // Map through the posts array to render each post card
+  if (error) {
+    return <p>Error: {error.message}</p>;
+  }
+
+  const posts = data.posts || [];
+
   return (
-    <div className="post-card flex bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-56 max-w-md md:max-w-2xl">
+    <div>
       <PostHeader />
-      <p className="text-gray-700"></p>
-      <p className="mt-3 text-gray-700 text-sm"></p>
-      <div className="mt-4 flex items-center">
-        <div className="flex mr-2 text-gray-700 text-sm mr-3">
-          <svg
-            fill="none"
-            viewBox="0 0 24 24"
-            className="w-4 h-4 mr-1"
-            stroke="currentColor"
-          >
-            {/* Rest of the SVG code */}
-          </svg>
-          <span>12</span>
+      {posts.map((post) => (
+        <div
+          key={post._id}
+          className="post-card flex bg-white shadow-lg rounded-lg mx-4 md:mx-auto my-56 max-w-md md:max-w-2xl"
+        >
+          <div>
+            <p className="text-gray-700">{post.createdAt}</p>
+            <p className="mt-3 text-gray-700 text-sm">{post.postText}</p>
+            <div className="mt-4 flex items-center">
+              {/* Rest of the component code */}
+            </div>
+          </div>
+          <p className="text-gray-700"></p>
+          <p className="mt-3 text-gray-700 text-sm"></p>
+          <div className="mt-4 flex items-center">
+            <div className="flex mr-2 text-gray-700 text-sm mr-3">
+              <svg
+                fill="none"
+                viewBox="0 0 24 24"
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+              >
+                {/* Rest of the SVG code */}
+              </svg>
+              <span>12</span>
+            </div>
+            <div className="flex mr-2 text-gray-700 text-sm mr-8">
+              <svg
+                fill="none"
+                viewBox="0 0 24 24"
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+              >
+                {/* Rest of the SVG code */}
+              </svg>
+              <span>8</span>
+            </div>
+            <div className="flex mr-2 text-gray-700 text-sm mr-4">
+              <svg
+                fill="none"
+                viewBox="0 0 24 24"
+                className="w-4 h-4 mr-1"
+                stroke="currentColor"
+              >
+                {/* Rest of the SVG code */}
+              </svg>
+              <span>share</span>
+            </div>
+          </div>
         </div>
-        <div className="flex mr-2 text-gray-700 text-sm mr-8">
-          <svg
-            fill="none"
-            viewBox="0 0 24 24"
-            className="w-4 h-4 mr-1"
-            stroke="currentColor"
-          >
-            {/* Rest of the SVG code */}
-          </svg>
-          <span>8</span>
-        </div>
-        <div className="flex mr-2 text-gray-700 text-sm mr-4">
-          <svg
-            fill="none"
-            viewBox="0 0 24 24"
-            className="w-4 h-4 mr-1"
-            stroke="currentColor"
-          >
-            {/* Rest of the SVG code */}
-          </svg>
-          <span>share</span>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
