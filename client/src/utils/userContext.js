40d7f06d511652a57001e9915106ceef252cd98a
Oscar Leal <oscarleal234@gmail.com>
2023-07-19 21:35:09 -0500

    worked on post component

---
+++ client/src/utils/userContext.js
@@ -0,0 +1,15 @@
+import React, { createContext, useState } from "react";
+
+const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [userData, setUserData] = useState(null);
+
+  return (
+    <UserContext.Provider value={{ userData, setUserData }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export { UserContext, UserProvider };
