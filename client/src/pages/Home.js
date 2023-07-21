e5acd4173078b49b07f795f41e21e461b9f6d021
Furly <michaelformico@gmail.com>
2023-07-21 15:33:41 -0700

    fixed first arrival to app pathing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,15 +2,25 @@ import React from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import CommunityContainer from "../components/CommunityContainer";
+import Auth from "../utils/auth";
+import { Navigate } from "react-router-dom";
+
 
 const Home = () => {
   return (
-    <div className="background-gray h-screen"> 
-      <Header />
-      <CommunityContainer />
-      <Footer />
+    <div>
+      {Auth.loggedIn() ? (
+        <>
+          <Header />
+          <CommunityContainer />
+          <Footer />
+        </>
+      ) : (
+        <Navigate to="/login" />
+      )}
     </div>
   );
 };
+
 
 export default Home;
