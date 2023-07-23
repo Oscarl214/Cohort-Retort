dba6be23e268c598d95b9c4cc316c324118cad77
Furly <michaelformico@gmail.com>
2023-07-23 13:36:34 -0700

    styled userinfo.js page for use on profile page, still need to finish post styling

--- client/src/components/UserInfo.js
+++ client/src/components/UserInfo.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useQuery } from "@apollo/client";
-
-// Import the custom user query
 import { QUERY_USER } from "../utils/queries";
+import PostHeader from "./PostComponents/PostHeader";
+import CreateComment from "../components/CreateComment";
+import Comment from "../components/Comment";
 
 const UserInfo = () => {
   const { data, loading, error } = useQuery(QUERY_USER);
@@ -15,82 +16,77 @@ const UserInfo = () => {
     return <p>Error: {error.message}</p>;
   }
 
-  const { user } = data; // Assuming the user data is under 'data.user' in the GraphQL response.
-  const { posts } = user; // Assuming the posts data is under 'user.posts' in the GraphQL response.
+  const { user } = data;
+  const { posts } = user;
 
   return (
-    <div className="max-w-md md:max-w-2xl mx-auto left-0">
-      {/* User Information */}
-      <div className="flex items-start px-4 py-6">
-        <img
-          className="w-12 h-12 rounded-full object-cover mr-4 shadow"
-          src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          alt="avatar"
-        />
-        <div>
-          <div className="flex items-center justify-between">
-            <h2 className="text-lg font-semibold text-gray-900 -mt-1">
+    <div className="bg-gray-200 min-h-screen">
+      <div className="mx-auto grid md:grid-cols-2 gap-4">
+        {/* User Information */}
+        <div className="bg-gray rounded-lg flex justify-center pt-20 max-h-96">
+          <div className="bg-white rounded-[5rem] max-w-2xl container relative py-10">
+            <p className="text-blue-900 text-center text-3xl font-bold">
               {user.firstName} {user.lastName}
-            </h2>
-            <small className="text-sm text-gray-700">{user.email}</small>
+            </p>
+            
+            <p className="text-blue-900 text-center">
+              <strong>Email:</strong>
+            </p>
+            <p className="text-gray-700 text-center">{user.email}</p>
+            
+            <p className="text-blue-900 text-center">
+              <strong>Website:</strong>
+            </p>
+            <p className="text-gray-700 text-center">{user.website}</p>
+           
+            <p className="text-blue-900 text-center">
+              <strong>LinkedIn:</strong>
+            </p>
+            <p className="text-gray-700 text-center">{user.linkedin}</p>
+            
+            <p className="text-blue-900 text-center">
+              <strong>GitHub:</strong>
+            </p>
+            <p className="text-gray-700 text-center">{user.github}</p>
           </div>
         </div>
-      </div>
 
-      {/* Posts */}
-      <div>
-        {posts.map((post) => (
-          <div
-            key={post._id}
-            className="post-card bg-white shadow-lg rounded-lg my-4 mx-4"
-          >
-            <div className="px-4 py-2">
-              <p className="text-gray-700">{post.createdAt}</p>
-              <p className="mt-3 text-gray-700 text-sm">{post.postText}</p>
-              <div className="mt-4 flex items-center">
-                {/* Rest of the component code */}
+        {/* Posts */}
+        <div className="p-20">
+          {user.posts.map((post) => (
+            <div key={post._id} className="bg-white shadow-lg rounded-xl mx-4 md:mx-auto max-w-md md:max-w-2xl my-6">
+              <PostHeader postID={post._id} />
+              <div className="pl-20 pr-8">
+                <p className="-mt-8 text-slate-400">{post.createdAt}</p>
+                <p className="mt-2 color-medblue text-sm">{post.postText}</p>
+              </div>
+
+              <div className="pl-20 pr-8 flex items-start mt-6 mb-6">
+                <div className="flex mr-2 text-gray-700 text-sm mr-3">
+                  <svg fill="none" viewBox="0 0 24 24" className="color-dkblue w-4 h-4 mr-1" stroke="currentColor">
+                    {/* Rest of the SVG code */}
+                  </svg>
+                  <span>63</span>
+                </div>
+
+                <div className="flex mr-2 text-gray-700 text-sm mr-8">
+                  <svg fill="none" viewBox="0 0 24 24" className="color-dkblue w-4 h-4 mr-1" stroke="currentColor">
+                    {/* Rest of the SVG code */}
+                  </svg>
+                  <span>4</span>
+                </div>
+
+                <CreateComment postID={post._id} />
+
+                <Comment postID={post._id} />
               </div>
             </div>
-            <div className="mt-4 flex items-center">
-              <div className="flex mr-2 text-gray-700 text-sm mr-3">
-                <svg
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                >
-                  {/* Rest of the SVG code */}
-                </svg>
-                <span>12</span>
-              </div>
-              <div className="flex mr-2 text-gray-700 text-sm mr-8">
-                <svg
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                >
-                  {/* Rest of the SVG code */}
-                </svg>
-                <span>8</span>
-              </div>
-              <div className="flex mr-2 text-gray-700 text-sm mr-4">
-                <svg
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="w-4 h-4 mr-1"
-                  stroke="currentColor"
-                >
-                  {/* Rest of the SVG code */}
-                </svg>
-                <span>share</span>
-              </div>
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   );
 };
 
 export default UserInfo;
+
