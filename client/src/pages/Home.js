fd312c3fc6f8362bc7ce6963afa23b63af159e69
Tim Mackie <timmackie177@gmail.com>
2023-07-22 19:23:43 -0500

    3rd try

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,5 @@
 import React from "react";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Nav from "../components/Nav";
 import CommunityContainer from "../components/CommunityContainer";
 import Auth from "../utils/auth";
 import { Navigate } from "react-router-dom";
@@ -8,16 +7,11 @@ import { Navigate } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="background-gray">
-      {Auth.loggedIn() ? (
-        <>
-          <Header />
-          <CommunityContainer />
-          <Footer />
-        </>
-      ) : (
-        <Navigate to="/login" />
-      )}
+    <div>
+      <Nav />
+      <CommunityContainer>
+        
+      </CommunityContainer>
     </div>
   );
 };
